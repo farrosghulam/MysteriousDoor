90850b0c4aed8dbc7629a5b3c6f82e0420ff3f28
farrosghulam <farrosghulam@gmail.com>
2026-09-25 13:58:43 +0700

    adding additional message if attempt is wrong

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@ while (attempt != secretCode)
 {
     Console.WriteLine("Enter the secret code to unlock the door:");
     attempt = Console.ReadLine();
+    if (attempt != secretCode)
+    {
+        Console.WriteLine("Wrong code! Try again.");
+    }
 }
 
 Console.WriteLine("The door is unlocked! Congratulations!");    
